3ebef3cf38209e9f576ad9ca1efdf3ee6bd2cc89
Jafari-mahdiCSF <m_jafari@kbtu.kz>
2025-06-24 10:35:52 +0500

    Update lab6.py

--- lab6.py
+++ lab6.py
@@ -137,3 +137,50 @@ if __name__ == "__main__":
     print("\n--- Delete file ---")
     delete_file(sample_copy)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+import os
+os.rmdir("myfolder")
+
+with open("demofile.txt", "a") as f:
+  f.write("Now the file has more content!")
+
+#open and read the file after the appending:
+with open("demofile.txt") as f:
+  print(f.read())
+
+
+with open("demofile.txt", "w") as f:
+  f.write("Woops! I have deleted the content!")
+
+#open and read the file after the overwriting:
+with open("demofile.txt") as f:
+  print(f.read())
+
+f = open("D:\\myfiles\welcome.txt")
+print(f.read())
+
+with open("demofile.txt") as f:
+  print(f.read())
+
